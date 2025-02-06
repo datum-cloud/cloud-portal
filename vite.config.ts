fd77d6b6d7f7aa2aad5783f9c01124bc43674727
Scot Wells <wells.scot@gmail.com>
2025-02-06 20:11:28 +0000

    fix: resolve server start issue
    Resolves an issue where the server isn't able to
    run after being build in production mode.
    
    See: https://github.com/remix-run/react-router/issues/12568

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,8 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': resolve(__dirname, './app'),
-    },
+			'react-dom/server': 'react-dom/server.node',
+		},
   },
   plugins: [
     remix({
