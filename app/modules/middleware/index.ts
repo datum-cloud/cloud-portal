90838e93e5e6939a26281ef7d73c740946dbbb84
Yahya Fakhroji <yahya.fakhroji@gmail.com>
2025-09-22 09:20:38 +0700

    feat: integrate middleware for policy bindings loader to enforce organization standards

---
+++ app/modules/middleware/index.ts
@@ -0,0 +1,7 @@
+export { authMiddleware } from './auth.middleware';
+export { withMiddleware } from './middleware';
+export {
+  createOrgTypeMiddleware,
+  standardOrgMiddleware,
+  personalOrgMiddleware,
+} from './org-type.middleware';
