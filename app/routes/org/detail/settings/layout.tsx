2d167af71296cf9fa6cae51aea31038d5bd31991
Yahya Fakhroji <yahya.fakhroji@gmail.com>
2025-09-22 08:50:27 +0700

    feat: hide "policy bindings" for personal org

--- app/routes/org/detail/settings/layout.tsx
+++ app/routes/org/detail/settings/layout.tsx
@@ -1,14 +1,17 @@
 import TabsLayout from '@/layouts/tabs/tabs';
 import { TabsNavProps } from '@/layouts/tabs/tabs.types';
+import { IOrganization, OrganizationType } from '@/resources/interfaces/organization.interface';
 import { paths } from '@/utils/config/paths.config';
 import { getPathWithParams } from '@/utils/helpers/path.helper';
 import { useMemo } from 'react';
-import { Outlet, useParams } from 'react-router';
+import { Outlet, useRouteLoaderData } from 'react-router';
 
 export default function OrgSettingsLayout() {
-  const { orgId } = useParams();
-  const navItems: TabsNavProps[] = useMemo(
-    () => [
+  const org = useRouteLoaderData<IOrganization>('org-detail');
+
+  const navItems: TabsNavProps[] = useMemo(() => {
+    const orgId = org?.name;
+    return [
       {
         value: 'preferences',
         label: 'Preferences',
@@ -18,15 +21,15 @@ export default function OrgSettingsLayout() {
         value: 'policy-bindings',
         label: 'Policy Bindings',
         to: getPathWithParams(paths.org.detail.settings.policyBindings, { orgId }),
+        hidden: org?.type === OrganizationType.Personal,
       },
       {
         value: 'activity',
         label: 'Activity',
         to: getPathWithParams(paths.org.detail.settings.activity, { orgId }),
       },
-    ],
-    [orgId]
-  );
+    ];
+  }, [org]);
   return (
     <TabsLayout tabsTitle={{ title: 'Organization Settings' }} navItems={navItems}>
       <Outlet />
