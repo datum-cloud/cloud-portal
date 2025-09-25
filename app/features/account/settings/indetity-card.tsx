4b6cca952ae48480f7a2bc1fd7351d79107291ed
Yahya Fakhroji <yahya.fakhroji@gmail.com>
2025-09-25 09:42:10 +0700

    chore: update identity card to display email address and replace Google icon with Mail icon

--- app/features/account/settings/indetity-card.tsx
+++ app/features/account/settings/indetity-card.tsx
@@ -1,7 +1,6 @@
-import GoogleIcon from '@/components/icon/google';
-import { Badge } from '@/components/ui/badge';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { useApp } from '@/providers/app.provider';
+import { MailIcon } from 'lucide-react';
 
 export const AccountIdentitySettingsCard = () => {
   const { user } = useApp();
@@ -14,13 +13,13 @@ export const AccountIdentitySettingsCard = () => {
       <CardContent>
         <div className="flex items-center justify-between gap-6">
           <div className="flex items-center gap-3">
-            <GoogleIcon className="size-5" />
+            <MailIcon className="size-5" />
             <div>
-              <div className="text-sm leading-5 font-semibold">Google</div>
+              <div className="text-sm leading-5 font-semibold">Email address</div>
               <div className="text-muted-foreground text-sm">{user?.email}</div>
             </div>
           </div>
-          <Badge variant="butter">Connected</Badge>
+          {/* <Badge variant="butter">Connected</Badge> */}
         </div>
       </CardContent>
     </Card>
