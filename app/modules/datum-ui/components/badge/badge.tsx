ad6ffdded3912e60dba3cd11efa9bede6b40e897
Dodik Gaghan <gaghan430@gmail.com>
2025-11-04 14:01:22 +0700

    Formatting code

--- app/modules/datum-ui/components/badge/badge.tsx
+++ app/modules/datum-ui/components/badge/badge.tsx
@@ -14,7 +14,8 @@ const badgeVariants = cva(
     variants: {
       variant: {
         default: 'border-transparent bg-primary text-primary-foreground hover:bg-primary/80',
-        secondary: 'border-transparent bg-sidebar text-primary dark:bg-card dark:text-sidebar-foreground',
+        secondary:
+          'border-transparent bg-sidebar text-primary dark:bg-card dark:text-sidebar-foreground',
         destructive:
           'border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80',
         outline: 'text-foreground',
