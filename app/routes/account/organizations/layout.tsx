09772109f0fbdb51ba987bb2831c6184f6a88134
Dodik Gaghan <gaghan430@gmail.com>
2025-11-19 17:42:59 +0700

    Hide breadcrumb in account org page

--- app/routes/account/organizations/layout.tsx
+++ app/routes/account/organizations/layout.tsx
@@ -3,10 +3,6 @@ import { mergeMeta, metaObject } from '@/utils/helpers/meta.helper';
 import { MetaFunction } from 'react-router';
 import { Outlet } from 'react-router';
 
-export const handle = {
-  breadcrumb: () => <span>Account Organizations</span>,
-};
-
 export const meta: MetaFunction = mergeMeta(() => {
   return metaObject('Account Organizations');
 });
