80e37dedf9f06c12ba4fb41788e082344a966be3
Matt Jenkinson <75292329+mattdjenkinson@users.noreply.github.com>
2025-12-16 17:14:20 +0000

    chore: update custom dialog not base shadcn

--- app/modules/shadcn/ui/dialog.tsx
+++ app/modules/shadcn/ui/dialog.tsx
@@ -27,7 +27,7 @@ const DialogOverlay = ({
     <DialogPrimitive.Overlay
       data-slot="dialog-overlay"
       className={cn(
-        'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 bg-dialog-overlay/50 fixed inset-0 z-50',
+        'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-black/80',
         className
       )}
       {...props}
