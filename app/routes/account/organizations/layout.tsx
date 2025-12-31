f81f435b9aa035317ec5b17ef5a7732287b52aef
Matt Jenkinson <75292329+mattdjenkinson@users.noreply.github.com>
2025-12-31 11:02:06 +0000

    chore: handle org loader errors better and handle layout post error

--- app/routes/account/organizations/layout.tsx
+++ app/routes/account/organizations/layout.tsx
@@ -1,11 +1,15 @@
 import { MinimalLayout } from '@/layouts/minimal.layout';
 import { mergeMeta, metaObject } from '@/utils/helpers/meta.helper';
-import { MetaFunction } from 'react-router';
+import { data, MetaFunction } from 'react-router';
 import { Outlet } from 'react-router';
 
 export const meta: MetaFunction = mergeMeta(() => {
   return metaObject('Account Organizations');
 });
+
+export const action = async () => {
+  return data({ error: 'Method not allowed on layout route' }, { status: 405 });
+};
 
 export default function Layout() {
   return (
