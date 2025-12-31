55877098bc50bae0981704fe5c9d2b9288a31e20
Matt Jenkinson <75292329+mattdjenkinson@users.noreply.github.com>
2025-12-31 11:11:06 +0000

    chore: add 405 action to root route

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,10 @@
 import { paths } from '@/utils/config/paths.config';
-import { redirect } from 'react-router';
+import { data, redirect } from 'react-router';
 
 export const loader = async () => {
   return redirect(paths.account.organizations.root);
+};
+
+export const action = async () => {
+  return data({ error: 'Method not allowed on index route' }, { status: 405 });
 };
